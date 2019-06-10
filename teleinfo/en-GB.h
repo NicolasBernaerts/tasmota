e59931da2aa26d097ab011d41ce39f46cad2daaa
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2019-06-10 15:35:08 +0200

    Update en-GB.h

--- teleinfo/en-GB.h
+++ teleinfo/en-GB.h
@@ -1,6 +1,10 @@
 /*
   en-GB.h - localization for English - United Kingdom for Sonoff-Tasmota
   Copyright (C) 2019  Theo Arends
+                2019  Nicolas Bernaerts
+
+  Integrates Teleinfo strings (xnrg_16_teleinfo.ino)
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
@@ -572,7 +576,7 @@
 #define D_SENSOR_TXD           "Serial Tx"
 #define D_SENSOR_RXD           "Serial Rx"
 #define D_SENSOR_ROTARY        "Rotary"     // Suffix "1A"
-#define D_SENSOR_TELEINFO_TX   "Teleinfo Tx"
+// Teleinfo
 #define D_SENSOR_TELEINFO_1200 "Teleinfo 1200"
 #define D_SENSOR_TELEINFO_9600 "Teleinfo 9600"
 
