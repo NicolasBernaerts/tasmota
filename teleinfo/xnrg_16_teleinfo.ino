2269aa3db839ef135f449bc23381493cd3949b94
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2019-07-06 23:33:09 +0200

    Add files via upload

--- teleinfo/xnrg_16_teleinfo.ino
+++ teleinfo/xnrg_16_teleinfo.ino
@@ -6,6 +6,7 @@
     05/05/2019 - v1.0 - Creation
     16/05/2019 - v1.1 - Add Tempo and EJP contracts
     08/06/2019 - v1.2 - Handle active and apparent power
+    05/07/2019 - v2.0 - Rework with selection thru web interface
     
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -48,11 +49,20 @@
 // default voltage (not provided by teleinfo)
 #define TELEINFO_VOLTAGE  230
 
+// icon coded in base64
+const char strIcon0[] PROGMEM = "iVBORw0KGgoAAAANSUhEUgAAAB0AAAAgCAMAAADZqYNOAAAJtHpUWHRSYXcgcHJvZmlsZSB0eXBlIGV4aWYAAHjarZhpliQpDoT/c4o5AptYjsP63tygjz+fwCPXyurqeR1eGXjgOAiZySTKrL/+u81/+Pgi0UTJJdWULJ9YY/WNm2Lv57bOxvN9f8znmfvcb/zTbz1dgTbcn2k94xv98v5Cjk9//9xv8njmKc9Ez4PXhEFX1tWeceWZKPjb757fpj7vtfhhO8/fWGcK655Jv/6OGWdMoTN441egn++iqwQsCDU02sR3CDrI0ep95tuF/GvfmVV+7by3uy++s+3pD59dYWx6BqQvPnr6nfzad8dDHy1y7yt/emC3bfbj54Pv9p5l73V312LCU8k8m3pt5dwxsOPKcF5LXJk/4T6fq3IVlhkgNkGzcw3jqvN4e7vopmtuu3Xa4QYmRr98pvV++HD6Ssi++nFAiXq57TPwTBMKWA1QC3T7N1vcWbee9YYrrDwdI71jMscb3y7zq87/53qbaG+lrnO2XD9BC+zySkDMUOT0m1EA4vbjUzn+PZf5wBv7AdgAgnLcXNhgs/1O0cW9cyscnAPjxEZjb2i4PJ8JcBFrC8a4AAI2uSAuOZu9z87hxwI+Dct9iL6DgBPx05kNNiEkwCle1+ad7M5YL/52Iy0AIQRNBhoCCLBiFPiTY4FDTQJ6JCJJshSp0lJIMUlKKSfVqJZDjllyyjmXXHMr";
+const char strIcon1[] PROGMEM = "ocQiJZVcSqmlVV8DEiY11WxqqbW2xqKNqRtvN0a01n0PPXbpqedeeu1tQJ8Rh4w08iijjjb9DJPwn2lmM8ussy23oNKKS1ZaeZVVV9twbYcdt+y08y677vaGmnvC9hNq7gtyv0fNPagpYvGMy++o0Z3zawqnciKKGYj56EA8KwIQ2itmtrgYvSKnmNnqCQrxoOZEwZlOEQPBuJyX7d6we0fut7gZif8IN/8Tckah+zeQMwrdg9x33H6B2mwno4QDkEah+tSGjbDtJM2X5rug9ecOEfyx3U6H2tR7quKX9NWynbGZOrAJdCWvzpK+p1RGWxm0CKXQ+hy+4e+FyDm3cnCrd520LYuXV84Rx/puxEX+lfb3tvy+NR+MDqntWI/xqzQ348pt7RRsqp3VZDYocRe2VnJfMxd325CMD7ziSl4t1Snp2BxCt2f6Zv+4NX87sA88MOs6LX1ZQu2OokSxPevOUK03b+Mn4wbjcW63baL2q2wJOK9haWpdSESznZ2JFg+fW/PTg7cWXk1ZNQhFgkwkstWxZa6Re5hOuM+oXjGjBY2Lte6bBI6T6gS3o6A5eWm96hRjz9xkktVkboJwOF+YceprgvIaJzuGmatIbkxORux4n8iSXuRxPlbEf+7sPmbruGTolKsMQijU1CM8SKkTuIkNzYhpUxcH+7HpMFZSwLSh45qAiN/NrcHOEv/6zCSRFLoL/VJIPNnnPoL7dRY0YmXvDJP2ppHf4Vu1HTlU";
+const char strIcon2[] PROGMEM = "kDSggie4jzf9llGRmpHK7Di7+zZXFI2oE1YtVBMX9k4rYttY7C4WvxjOoMMTatFe0bQehFlDa9UyNRJYTgTshGfpRdjwJGtoOPRVLnLoWstlSA7AjD1sbtURCSF8IJeFzb+5n+KFmDeKIJnSrm92vHxZoWKrB9BDH8H+jppeWlNqHqCMznnCFBmBxQusIPeuGLCCZRxsZwvC2Jp72p4YQOPqiuBxmBWmtIFCjjFVOH0/2yLnvziNxq6hhOZvOTQ9rZJ2xDplK0NuyDzMNwGf1IjMeVfHcmPkEmfKVx1cF8ro1tluBsIquHMxXHADYxJxfJcNqkd04NHGiuIVWsKVxxupL4TR4Q7xUGWsHdnDIbunlnujcBsQxqio+YHD1XukUiw8Y6neIbl4sPSoKg5sZexLcg97x1RqL4jWibXcjbovXPdlnNH7YeM6sjGbdOIMxSO9oPLuLtKpjN6FFYapDUaNkFoZgIWk2eGhfEo8bGqdncXf7QlcHM9brUe2r7x626cKm4YFdqcVRyOumpIeN6rXVrebaR3wbwyf7RIaklCHReQvdthHJiwmy8Uy+TTcVUK03en+2lQWSD03R/Ddz6352qFEVj7UHSp8yKMlqxTAQSTjHcuyJzZVH99JL5nT0RvnjxJNjy1jafRRctQRatm2akBvnM+BDZFyuhzSQ/4U6BKXTc0MjSjyK0cCImQuxgJwQhioJ15u8Y0gEfIUXiajTLI7GftTpBt80/5A+Kl9";
+const char strIcon3[] PROGMEM = "phQl/Ev6CTNAU9d3kvswYe5Ww5qLBMQIgldVNgFZKNP1zJTsFwUiqG/KzVGxxazLpVrK2q0bCok+KIC0LTfxTU3XFyk0qaKxBCJhrvR3VSimIpXPE5LxSTDGH0rfyVuBx72Pl58odypyvRaTPnPlOn0hECLl5EcVd+YLI77J+peFv6z77lWjVQY1bEJ64IjKWrk5GjKc+oeTNSY8O9X8+bz7YYkCGqa2RA1J9jpWRv+KxjD9urukHHCpF+rII+8ZRbhbKq6/b8V8ZrtTvHUBTWwUtD0TYlUBDYFtQri5U0dgoMPaHGh5BLJhJKN5hQhj+yRqFrVYtZWsbHJrAiKmV75U1lo8K5WPhwilmMr3akSlZnI/9dh7osgGAhrslV9E21bBRWge6ReynRY+QBkIN4MRPoVrxZN0kQk5fiazap35gPz7ktf8pLR+PIXgH1R/KtomfRNtubnvT+rZXgLWayEkRvEAlgPuPLnwUcB0FbA9CngE8IgV8pQRzkDeVUzvS974pqFa3/TBry+FwKMOQvUhPG6yJpUPKh/JXTXdUKdgV3HGG4U1tETOjzpnTSVXCDoy4CkTOC+InpBu9aRVBvnwZZyYY52qImmpqbRsDifUN7uNXN/fyZnS8sm12amYIhP9ViYjkmrMD0joMWANlGWeppHTI8mxUjCTDLSLaot6Lp36qydvWjnVx+v4oK1m7gb/s5bCnH/IpwTEbWPiqMAZltubu1FMrcyH8agGmk9e";
+const char strIcon4[] PROGMEM = "7I3t4mgZURFkwUKZs5euffMGiS6nrsejdw1OGg0saICCICLThpmGfnvXA9FOmah5G3GbnCwn504fdJ86MTIN6I1Bs+hRVRXOnGJjcMZ9xf9wk73pPuFm310jOQBAANRctTbWHUo4aNgDWhglma66QyTiFiCpKtgE6ElX28828SzlA8z3p0zySgV0o2RlrfrIa1imalT+tC5l33PoaSCcFyLMS3rKVPizauPu0e0edAp/z5AbEVlDS2g8ZR5Dm56ewOPkajeClj6L0gL3aPm9KTjGcZpOwRsocyZ3MzBTIsINA5JZ/08CMAAY5bt2kZwocZ1z17B331A1UWWVuhNqSk7R4ClkO0P1RREfcCb9m+N4Re3+B/orwRn8e1zgAAAAe1BMVEWQnuJOgb1Og7w/ia1Ig7JCi6lKgrtCi6lAhLhCi6lNgb1Kfr9Ogb3///86apNEdqn9/v9KgLlWhsD1+Puhu9xKe7Q8bZfp7/b7/P2zyN+Qrs7Q3eq/0eVAcaJ/osfe5/A4apNul8ZLeJ5dh6g/cJs6apVlkL6ku884aJFDET/aAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAHdElNRQfjBh4LJRMDmmhYAAAA7klEQVQoz6VS0Q7DIAj0C3ycEQxmsIj//4VD66Zb127JSEyEq3B31LklWITVHYSvtaZwAlY8AKV+Rw/HdjS5s8d5X57wdUe5";
+const char strIcon5[] PROGMEM = "+DJuGvtTU83CwwLrJ6sldQs/ydi3slJvcWnp7ZHRaK8KZfIfrfwUE0K6VF82/tBHvzoMmsIoBMQcjux/Fc9ndZtyk48b7KypCfy0Xnra4deeRXgq7n7w3jxdVglvi6+Sl18I3lZLOCSjEeO4TMYYcfFDNf9kzr8h4uix/dKyfkZQAqTcvWAkS5US4URLpjBQuyaFNMQT0RVNQwBtkUkjqlqFCM753AEIHyqOp8BqRwAAAABJRU5ErkJggg==";
+const char *const arrIconBase64[] PROGMEM = {strIcon0, strIcon1, strIcon2, strIcon3, strIcon4, strIcon5};
+
 // teleinfo data
 bool  teleinfo_configured = false;
 bool  teleinfo_enabled    = false;
-int   teleinfo_adps    = 0;
-int   teleinfo_counter = 0;
+int   teleinfo_adps       = 0;
+int   teleinfo_counter    = 0;
 char* teleinfo_message_buffer  = NULL;
 char* teleinfo_message_temp    = NULL;
 char* teleinfo_message_json    = NULL;
@@ -72,32 +82,34 @@ uint32_t teleinfo_bbrhpjr = 0;
 uint32_t teleinfo_ejphn   = 0;
 uint32_t teleinfo_ejphpm  = 0;
 
-// icon coded in base64
-const char arrIconBase64[] PROGMEM = "iVBORw0KGgoAAAANSUhEUgAAAB0AAAAgCAMAAADZqYNOAAAJtHpUWHRSYXcgcHJvZmlsZSB0eXBlIGV4aWYAAHjarZhpliQpDoT/c4o5AptYjsP63tygjz+fwCPXyurqeR1eGXjgOAiZySTKrL/+u81/+Pgi0UTJJdWULJ9YY/WNm2Lv57bOxvN9f8znmfvcb/zTbz1dgTbcn2k94xv98v5Cjk9//9xv8njmKc9Ez4PXhEFX1tWeceWZKPjb757fpj7vtfhhO8/fWGcK655Jv/6OGWdMoTN441egn++iqwQsCDU02sR3CDrI0ep95tuF/GvfmVV+7by3uy++s+3pD59dYWx6BqQvPnr6nfzad8dDHy1y7yt/emC3bfbj54Pv9p5l73V312LCU8k8m3pt5dwxsOPKcF5LXJk/4T6fq3IVlhkgNkGzcw3jqvN4e7vopmtuu3Xa4QYmRr98pvV++HD6Ssi++nFAiXq57TPwTBMKWA1QC3T7N1vcWbee9YYrrDwdI71jMscb3y7zq87/53qbaG+lrnO2XD9BC+zySkDMUOT0m1EA4vbjUzn+PZf5wBv7AdgAgnLcXNhgs/1O0cW9cyscnAPjxEZjb2i4PJ8JcBFrC8a4AAI2uSAuOZu9z87hxwI+Dct9iL6DgBPx05kNNiEkwCle1+ad7M5YL/52Iy0AIQRNBhoCCLBiFPiTY4FDTQJ6JCJJshSp0lJIMUlKKSfVqJZDjllyyjmXXHMrocQiJZVcSqmlVV8DEiY11WxqqbW2xqKNqRtvN0a01n0PPXbpqedeeu1tQJ8Rh4w08iijjjb9DJPwn2lmM8ussy23oNKKS1ZaeZVVV9twbYcdt+y08y677vaGmnvC9hNq7gtyv0fNPagpYvGMy++o0Z3zawqnciKKGYj56EA8KwIQ2itmtrgYvSKnmNnqCQrxoOZEwZlOEQPBuJyX7d6we0fut7gZif8IN/8Tckah+zeQMwrdg9x33H6B2mwno4QDkEah+tSGjbDtJM2X5rug9ecOEfyx3U6H2tR7quKX9NWynbGZOrAJdCWvzpK+p1RGWxm0CKXQ+hy+4e+FyDm3cnCrd520LYuXV84Rx/puxEX+lfb3tvy+NR+MDqntWI/xqzQ348pt7RRsqp3VZDYocRe2VnJfMxd325CMD7ziSl4t1Snp2BxCt2f6Zv+4NX87sA88MOs6LX1ZQu2OokSxPevOUK03b+Mn4wbjcW63baL2q2wJOK9haWpdSESznZ2JFg+fW/PTg7cWXk1ZNQhFgkwkstWxZa6Re5hOuM+oXjGjBY2Lte6bBI6T6gS3o6A5eWm96hRjz9xkktVkboJwOF+YceprgvIaJzuGmatIbkxORux4n8iSXuRxPlbEf+7sPmbruGTolKsMQijU1CM8SKkTuIkNzYhpUxcH+7HpMFZSwLSh45qAiN/NrcHOEv/6zCSRFLoL/VJIPNnnPoL7dRY0YmXvDJP2ppHf4Vu1HTlUkDSggie4jzf9llGRmpHK7Di7+zZXFI2oE1YtVBMX9k4rYttY7C4WvxjOoMMTatFe0bQehFlDa9UyNRJYTgTshGfpRdjwJGtoOPRVLnLoWstlSA7AjD1sbtURCSF8IJeFzb+5n+KFmDeKIJnSrm92vHxZoWKrB9BDH8H+jppeWlNqHqCMznnCFBmBxQusIPeuGLCCZRxsZwvC2Jp72p4YQOPqiuBxmBWmtIFCjjFVOH0/2yLnvziNxq6hhOZvOTQ9rZJ2xDplK0NuyDzMNwGf1IjMeVfHcmPkEmfKVx1cF8ro1tluBsIquHMxXHADYxJxfJcNqkd04NHGiuIVWsKVxxupL4TR4Q7xUGWsHdnDIbunlnujcBsQxqio+YHD1XukUiw8Y6neIbl4sPSoKg5sZexLcg97x1RqL4jWibXcjbovXPdlnNH7YeM6sjGbdOIMxSO9oPLuLtKpjN6FFYapDUaNkFoZgIWk2eGhfEo8bGqdncXf7QlcHM9brUe2r7x626cKm4YFdqcVRyOumpIeN6rXVrebaR3wbwyf7RIaklCHReQvdthHJiwmy8Uy+TTcVUK03en+2lQWSD03R/Ddz6352qFEVj7UHSp8yKMlqxTAQSTjHcuyJzZVH99JL5nT0RvnjxJNjy1jafRRctQRatm2akBvnM+BDZFyuhzSQ/4U6BKXTc0MjSjyK0cCImQuxgJwQhioJ15u8Y0gEfIUXiajTLI7GftTpBt80/5A+Kl9phQl/Ev6CTNAU9d3kvswYe5Ww5qLBMQIgldVNgFZKNP1zJTsFwUiqG/KzVGxxazLpVrK2q0bCok+KIC0LTfxTU3XFyk0qaKxBCJhrvR3VSimIpXPE5LxSTDGH0rfyVuBx72Pl58odypyvRaTPnPlOn0hECLl5EcVd+YLI77J+peFv6z77lWjVQY1bEJ64IjKWrk5GjKc+oeTNSY8O9X8+bz7YYkCGqa2RA1J9jpWRv+KxjD9urukHHCpF+rII+8ZRbhbKq6/b8V8ZrtTvHUBTWwUtD0TYlUBDYFtQri5U0dgoMPaHGh5BLJhJKN5hQhj+yRqFrVYtZWsbHJrAiKmV75U1lo8K5WPhwilmMr3akSlZnI/9dh7osgGAhrslV9E21bBRWge6ReynRY+QBkIN4MRPoVrxZN0kQk5fiazap35gPz7ktf8pLR+PIXgH1R/KtomfRNtubnvT+rZXgLWayEkRvEAlgPuPLnwUcB0FbA9CngE8IgV8pQRzkDeVUzvS974pqFa3/TBry+FwKMOQvUhPG6yJpUPKh/JXTXdUKdgV3HGG4U1tETOjzpnTSVXCDoy4CkTOC+InpBu9aRVBvnwZZyYY52qImmpqbRsDifUN7uNXN/fyZnS8sm12amYIhP9ViYjkmrMD0joMWANlGWeppHTI8mxUjCTDLSLaot6Lp36qydvWjnVx+v4oK1m7gb/s5bCnH/IpwTEbWPiqMAZltubu1FMrcyH8agGmk9e7I3t4mgZURFkwUKZs5euffMGiS6nrsejdw1OGg0saICCICLThpmGfnvXA9FOmah5G3GbnCwn504fdJ86MTIN6I1Bs+hRVRXOnGJjcMZ9xf9wk73pPuFm310jOQBAANRctTbWHUo4aNgDWhglma66QyTiFiCpKtgE6ElX28828SzlA8z3p0zySgV0o2RlrfrIa1imalT+tC5l33PoaSCcFyLMS3rKVPizauPu0e0edAp/z5AbEVlDS2g8ZR5Dm56ewOPkajeClj6L0gL3aPm9KTjGcZpOwRsocyZ3MzBTIsINA5JZ/08CMAAY5bt2kZwocZ1z17B331A1UWWVuhNqSk7R4ClkO0P1RREfcCb9m+N4Re3+B/orwRn8e1zgAAAAe1BMVEWQnuJOgb1Og7w/ia1Ig7JCi6lKgrtCi6lAhLhCi6lNgb1Kfr9Ogb3///86apNEdqn9/v9KgLlWhsD1+Puhu9xKe7Q8bZfp7/b7/P2zyN+Qrs7Q3eq/0eVAcaJ/osfe5/A4apNul8ZLeJ5dh6g/cJs6apVlkL6ku884aJFDET/aAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAHdElNRQfjBh4LJRMDmmhYAAAA7klEQVQoz6VS0Q7DIAj0C3ycEQxmsIj//4VD66Zb127JSEyEq3B31LklWITVHYSvtaZwAlY8AKV+Rw/HdjS5s8d5X57wdUe5+DJuGvtTU83CwwLrJ6sldQs/ydi3slJvcWnp7ZHRaK8KZfIfrfwUE0K6VF82/tBHvzoMmsIoBMQcjux/Fc9ndZtyk48b7KypCfy0Xnra4deeRXgq7n7w3jxdVglvi6+Sl18I3lZLOCSjEeO4TMYYcfFDNf9kzr8h4uix/dKyfkZQAqTcvWAkS5US4URLpjBQuyaFNMQT0RVNQwBtkUkjqlqFCM753AEIHyqOp8BqRwAAAABJRU5ErkJggg==";
-
 /*********************************************************************************************/
 
 // get teleinfo mode (baud rate)
-int TeleinfoGetMode ()
+uint16_t TeleinfoGetMode ()
 {
-  int actual_mode;
+  uint16_t actual_mode;
 
   // read actual teleinfo mode
-  actual_mode = (int) (Settings.sbaudrate * 1200);
+  actual_mode = (uint16_t) (Settings.sbaudrate * 1200);
 
   // if outvalue, set to disabled
   if ((actual_mode != 1200) && (actual_mode != 9600)) actual_mode = 0;
-
+  
   return actual_mode;
 }
 
 // set teleinfo mode (baud rate)
-void TeleinfoSetMode (int new_mode)
+void TeleinfoSetMode (uint16_t new_mode)
 {
-  uint8_t new_speed;
-  
   // if within range, set baud rate
-  if ((new_mode == 0) || (new_mode == 1200) || (new_mode == 9600)) Settings.sbaudrate = (uint8_t) (new_mode / 1200);
+  if ((new_mode == 0) || (new_mode == 1200) || (new_mode == 9600))
+  {
+    // save mode
+    Settings.sbaudrate = (uint8_t) (new_mode / 1200);
+
+    // ask for restart
+    restart_flag = 2;
+  }
 }
 
 void TeleinfoEvery200ms ()
@@ -263,12 +275,13 @@ bool TeleinfoPreInit ()
       snprintf_P (teleinfo_message_buffer, sizeof(teleinfo_message_buffer), "");
     }
   }
+  
   return teleinfo_configured;
 }
 
 void TeleinfoInit ()
 {
-  int teleinfo_mode;
+  uint16_t teleinfo_mode;
 
   // get teleinfo speed
   teleinfo_mode = TeleinfoGetMode ();
@@ -276,6 +289,10 @@ void TeleinfoInit ()
   // if sensor has been pre initialised
   if ((teleinfo_configured == true) && (teleinfo_mode > 0))
   {
+    // set energy voltage
+    energy_voltage = TELEINFO_VOLTAGE;
+    RtcSettings.energy_kWhtotal = 0;
+
     // set serial port
     teleinfo_serial = new TasmotaSerial (pin[GPIO_RXD], pin[GPIO_TXD], 1);
     
@@ -286,12 +303,11 @@ void TeleinfoInit ()
     // check port allocated
     teleinfo_enabled = teleinfo_serial->hardwareSerial ();
     if ( teleinfo_enabled == true) ClaimSerial ();
-    else energy_flg = ENERGY_NONE;
-
-    // set energy voltage
-    energy_voltage = TELEINFO_VOLTAGE;
-    RtcSettings.energy_kWhtotal = 0;
   }
+
+  // if teleinfo is not enabled, reset energy flag
+  if ( teleinfo_enabled == false) energy_flg = ENERGY_NONE;
+
 }
 
 void TeleinfoJSONAppend ()
@@ -325,13 +341,18 @@ void TeleinfoLoop ()
 
 #ifdef USE_WEBSERVER
 
-// VMC icon
+// Display Teleinfo icon
 void TeleinfoWebDisplayIcon (uint8_t height)
 {
+  uint8_t nbrItem, index;
+
+  // display img according to height
   WSContentSend_P ("<img height=%d src='data:image/png;base64,", height);
-  
-  WSContentSend_P (arrIconBase64);
-  
+
+  // loop to display base64 segments
+  nbrItem = sizeof (arrIconBase64) / sizeof (char*);
+  for (index=0; index<nbrItem; index++) { WSContentSend_P (arrIconBase64[index]); }
+
   WSContentSend_P ("'/>");
 }
 
@@ -377,7 +398,7 @@ void TeleinfoWebMainButton ()
   // beginning
   WSContentSend_P (PSTR ("<table style='width:100%%;padding:5px 10px;'><tr>"));
 
-  // vmc icon
+  // teleinfo icon
   WSContentSend_P (PSTR ("<td>"));
   TeleinfoWebDisplayIcon (32);
   WSContentSend_P (PSTR ("</td>"));
@@ -399,17 +420,14 @@ void TeleinfoWebPage ()
   // if access not allowed, close
   if (!HttpCheckPriviledgedAccess()) return;
 
-  // get VMC mode according to MODE parameter
+  // get teleinfo mode according to MODE parameter
   if (WebServer->hasArg(D_CMND_TELEINFO_MODE))
   {
     WebGetArg (D_CMND_TELEINFO_MODE, argument, TELEINFO_MESSAGE_BUFFER_SIZE);
     TeleinfoSetMode ((uint16_t) atoi (argument)); 
   }
-
-  // back to main page
-  WebServer->sendHeader ("Location", "/", true);
-  WebServer->send ( 302, "text/plain", "");
 }
+
 #endif  // USE_WEBSERVER
 
 /*********************************************************************************************\
